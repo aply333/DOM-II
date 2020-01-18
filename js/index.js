4fa8a3b8f4cee301ef2fc333dbafea54bf08b829
Andrei Lysenko <andrei.lysenko@outlook.com>
2020-01-17 23:41:42 -0800

    added some events, but still need to add more

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+let navLinks = document.querySelectorAll('.nav-link');
+let letGoImg = document.querySelector('.img-content img');
+let logoHeading = document.querySelector('.logo-heading');
+let introH2 = document.querySelector('.intro h2');
+let body = document.querySelector('body');
+let introPara = document.querySelector('.intro p');
+
+
+// navLinks.forEach((cV, index) =>{})
+
+letGoImg.addEventListener('dblclick', () => {
+    letGoImg.setAttribute('src', './img/fun-bus.jpg')
+})
+
+logoHeading.addEventListener('select', () => {
+    introH2.style.color = 'green';
+})
+
+body.addEventListener('keydown', () =>{
+    body.style.backgroundColor = "red";
+})
+logoHeading.addEventListener('mouseover', () =>{
+    introH2.style.color = 'white';
+})
+body.addEventListener('wheel', () => {
+    introH2.style.backgroundColor = 'purple';
+})
+body.addEventListener('keyup', ()=>{
+    body.style.backgroundColor = "grey";
+})
+body.addEventListener('contextmenu', () =>{
+    introPara.textContent = 'A developer whose goal is one massive obstruction to any thought of design.'
+})
